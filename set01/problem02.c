9892a00717d9383c7186ded5754128deb84036b6
HARSHINI20070808 <sharshini219@gmail.com>
2026-09-16 16:42:41 +0530

    Update problem02.c

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,14 @@
 //2. Write a C program to add two numbers.
+# include <stdio.h>
+int main()
+{
+  a=8;
+  b=1;
+  c=a+b;
+  printf("%d",c);
+  printf("sum=%d",c);
+  printf("sum of %d and %d =%d",a,b,c);
+  return 0;
+}
+
 
